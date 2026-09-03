da82a03e8614c62660d7fa32871fbef5b2a36c5a
Ganesh Sable <ganeshsable1757@gmail.com>
2026-09-03 22:38:05 +0530

    Added Batch Normalization

---
+++ ANN/05_Batch_Normalization/with_batchNorm.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+data = fetch_california_housing()
+
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size= 0.2,
+    random_state= 42
+)
+
+print("\nTrain shape:", X_train.shape)
+print("Test shape:", X_test.shape)
+
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+X_train = torch.tensor(
+    X_train,
+    dtype = torch.float32
+)
+
+y_train = torch.tensor(
+    y_train,
+    dtype = torch.float32
+).reshape(-1, 1)
+
+X_test = torch.tensor(
+    X_test,
+    dtype = torch.float32
+)
+
+y_test = torch.tensor(
+    y_test,
+    dtype = torch.float32
+).reshape(-1, 1)
+
+
+print("\nTensor shapes:")
+print("X_train:", X_train.shape)
+print("y_train:", y_train.shape)
+print("X_test:", X_test.shape)
+print("y_test:", y_test.shape)
+
+class housingModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.layer1 = nn.Linear(8, 64)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.layer2 = nn.Linear(64, 32)
+        self.output = nn.Linear(32, 1)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.bn1(x)
+        x = torch.relu(x)
+        x = self.layer2(x)
+        x = self.output(x)
+
+        return x
+
+model = housingModel()
+
+loss_fun = nn.MSELoss()
+
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr= 0.001
+)
+
+train_dataset = TensorDataset(X_train, y_train)
+test_dataset = TensorDataset(X_test, y_test)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size= 32,
+    shuffle= True
+)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size= 32
+)
+
+
+
+for epoch in range(50):
+    total_loss = 0
+    model.train()
+
+    for batch_x, batch_y in train_loader:
+
+        y_pred = model(batch_x)
+
+        loss = loss_fun(y_pred, batch_y)
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        total_loss += loss.item() 
+
+    if epoch % 10 == 0:
+        average_loss = total_loss / len(train_loader)
+        print(f"Epoch: {epoch}, Loss: {average_loss:.4f}")
+
+
+model.eval()
+
+total_test_loss = 0
+
+with torch.no_grad():
+    for batch_x, batch_y in test_loader:
+        prediction = model(batch_x)
+
+        loss = loss_fun(prediction, batch_y)
+
+        total_test_loss += loss.item()
+        
+        
+average_test_loss = (
+    total_test_loss / len(test_loader)
+    )
+        
+print("\nTest Loss:", average_test_loss)
+
+
+
+new_data = X_test[0]
+
+new_data = new_data.reshape(1, -1)
+
+
+model.eval()
+
+with torch.no_grad():
+
+    prediction = model(new_data)
+
+print(
+    "Predicted Value:",
+    prediction.item()
+)
+
+print(
+    "Actual Value:",
+    y_test[0].item()
+)
